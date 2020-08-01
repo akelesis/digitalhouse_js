5d277448808c6fd8f0396b17797946a63806f673
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2020-08-01 19:38:20 -0300

    Adding Filter By Book's Category

--- main.js
+++ main.js
@@ -68,7 +68,8 @@ let imgCarousel = function () {
 };
 
 // Show the carousel only when the page finishes loading
-window.onload = imgCarousel();
+// window.onload = imgCarousel();
+imgCarousel();
 
 // Filling the books
 let fillCards = function () {
@@ -76,50 +77,52 @@ let fillCards = function () {
     let divCard = document.querySelectorAll("div.card");
     let cardImg = document.querySelectorAll("div.card img");
     let titleCard = document.querySelectorAll("div.overlay h2");
-    let pCard = document.querySelectorAll("div.overlay p");
-
+    let pResume = document.querySelectorAll("div.overlay p.resume");
+    let pCategory = document.querySelectorAll("div.overlay p.category");
+    let pPrice = document.querySelectorAll("div.overlay p.price");
+    
     let books = [
         {
             img: 'https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L.jpg',
             title: 'Clean Code',
             abstract: 'Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.',
-            category: '',
-            price: ''
+            category: 'Auto Ajuda',
+            price: '40.0'
         },
         {
             img: 'https://images-na.ssl-images-amazon.com/images/I/51sZW87slRL._SX375_BO1,204,203,200_.jpg',
             title: 'Domain-Driven Design',
             abstract: 'Domain-Driven Design fills that need. This is not a book about specific technologies. It offers readers a systematic approach to domain-driven design, presenting an extensive set of design best practices, experience-based techniques, and fundamental principles that facilitate the development of software projects facing complex domains.',
-            category: '',
-            price: ''
+            category: 'Fantasia',
+            price: '20.0'
         },
         {
             img: 'https://images-na.ssl-images-amazon.com/images/I/516tg-5b2ZL._SX380_BO1,204,203,200_.jpg',
             title: 'Clean Architecture',
             abstract: 'Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.',
-            category: '',
-            price: ''
+            category: 'Auto Ajuda',
+            price: '60.0'
         },
         {
             img: 'https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L.jpg',
             title: 'Clean Code',
             abstract: 'Robert C. Martin (“Uncle Bob”) has been a programmer since 1970. He is founder of Uncle Bob Consulting, LLC, and cofounder with his son Micah Martin of The Clean Coders LLC. Martin has published dozens of articles in various trade journals and is a regular speaker at international conferences and trade shows.',
-            category: '',
-            price: ''
+            category: 'Romance',
+            price: '10.0'
         },
         {
             img: 'https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L.jpg',
             title: 'Cracking the Coding Interview',
-            abstract: 'Gayle Laakmann McDowell is the founder and CEO of CareerCup and the author of Cracking the PM Interview and Cracking the Tech Career. Her background is in software development. She has worked as a software engineer at Google, Microsoft, and Apple. At Google, she interviewed hundreds of software engineers and evaluated thousands of hiring packets on the hiring committee. She holds a B.S.E. and M.S.E. in computer science from the University of Pennsylvania and an MBA from the Wharton School.',
-            category: '',
-            price: ''
+            abstract: 'Gayle Laakmann McDowell is the founder and CEO of CareerCup and the author of Cracking the PM Interview and Cracking the Tech Career. Her background is in software development. She has worked as a software engineer at Google, Microsoft, and Apple.',
+            category: 'Ficção Científica',
+            price: '150.0'
         },
         {
             img: 'https://images-na.ssl-images-amazon.com/images/I/41vEGoXo1sL._SX323_BO1,204,203,200_.jpg',
             title: 'Javascript e Jquery',
             abstract: 'Jon Duckett projeta e desenvolve websites há mais de quinze anos. Trabalhou em pequenas startups e empresas internacionais e escreveu mais de uma dúzia de livros sobre web design e programação.',
-            category: '',
-            price: ''
+            category: 'Romance',
+            price: '200.0'
         }
     ];
 
@@ -127,15 +130,17 @@ let fillCards = function () {
     for (let i = 0; i < divCard.length; i++) {
         cardImg[i].setAttribute('src', books[i].img);
         titleCard[i].textContent = books[i].title;
-        pCard[i].textContent = books[i].abstract;
+        pResume[i].textContent = books[i].abstract;
+        pCategory[i].textContent = books[i].category;
+        pPrice[i].textContent = books[i].price;
     }
 
 };
 
-window.onload = fillCards();
+fillCards();
 
 // Making a book filter
-let searchingBooks = function() {
+let searchingBooks = function () {
     let btnSearch = document.getElementById('search_button');
     let inputSearch = document.getElementById('search_bar');
 
@@ -147,27 +152,31 @@ let searchingBooks = function() {
     };
 
     // Catch and pass the text when the enter is pressed if isn't empty
-    inputSearch.addEventListener('keyup', function(event) {
+    inputSearch.addEventListener('keyup', function (event) {
         if ((event.keyCode === 13) && (inputSearch.value != '')) {
             searchingBooks(inputSearch.value);
         }
     });
-    
-    // Receive the text and does the search
+
+    // Receive the property and does the search
     searchingBooks = function (input) {
         let getCards = document.querySelectorAll('.card');
-    
+
         // Scrolls through the cards and changes their display
         for (let i = 0; i < getCards.length; i++) {
             let getTitle = getCards[i].querySelector('h2').textContent || getCards[i].querySelector('h2').innerText;
-    
-            if (getTitle.toUpperCase() == input.toUpperCase()) {
+
+            let getCategory = getCards[i].querySelector('.category').textContent || getCards[i].querySelector('.category').innerText;
+
+            if ((getTitle.toUpperCase() == input.toUpperCase())
+             || (getCategory == input )) {
                 getCards[i].style.display = '';
-            } else  getCards[i].style.display = 'none';
+            } else getCards[i].style.display = 'none';
+
         }
 
         // If the user clears the input label, shows all cards again
-        inputSearch.addEventListener('keyup', function(event) {
+        inputSearch.addEventListener('keyup', function (event) {
             if (event.keyCode === 8) {
                 let timePressed = 0;
 
@@ -178,8 +187,31 @@ let searchingBooks = function() {
                 }
             }
         });
-    
+
     };
+
+
+    let searchingByCategory = function () {
+        let categoryField = document.getElementById("category");
+        categoryField.onclick = function () {
+            if(categoryField.value != '') {
+                searchingBooks(categoryField.value);
+            }
+        }
+    };
+    searchingByCategory();
+
+
+    // NOTE: parei aqui
+    let searchingByPrice = function () {
+        let priceField = document.getElementById("price_range");
+        priceField.onclick = function () {
+            if(priceField.value != '') {
+                searchingBooks(priceField.value);
+            }
+        }
+    };
+    searchingByPrice();
 }
 
 searchingBooks();
@@ -188,4 +220,12 @@ searchingBooks();
 
 
 
-
+/*
+ TODO:
+- Verificar side menu em mobile;
+- Baixar as imagens pro projeto;
+- Ao clicar no card, mostra as informações do livro;
+- REAPROVEITAR OS CÓDIGOSSSSSS
+- SAIR DA PROCURA POR CATEGORIA/PREÇO
+.
+*/
