e501a0c6142209038db826a32225d2a904ad574e
Ágata <agatagabriele20@gmail.com>
2020-08-01 20:55:05 -0300

    carrousell

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("menu").style.width = "0px";
 }
+
+$('#dh_carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 3000,
+    autoplay: true
+  });
