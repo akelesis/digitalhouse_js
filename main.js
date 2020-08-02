3ed8644a608c5ec0ea5f1d6e1f4ee4aa0a6c1008
Maik Henrique <maik.hsp.contato@gmail.com>
2020-08-02 12:12:21 -0300

    Implemented carousel functionality

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const burgerButton = document.querySelector("#dh_menu_btn");
 const sideMenu = document.querySelector(".side-menu");
 
+const carouselSlider = document.querySelector(".carousel-slider");
+const carouselImages = document.querySelectorAll(".carousel-slider img");
+const nextButton = document.querySelector("#next-button");
+const previousButton = document.querySelector("#previous-button");
+const carouselWidth = carouselSlider.clientWidth;
+let slideFactor = 0;
+
 burgerButton.addEventListener("click", showSideMenu);
 window.addEventListener("scroll", hideSideMenuScroll);
+
+nextButton.addEventListener("click", slideForward);
+previousButton.addEventListener("click", slideBackward);
 
 function showSideMenu(event) {
     console.log(sideMenu);
@@ -13,4 +23,17 @@ function hideSideMenuScroll(event) {
     if (sideMenu.classList.contains("side-menu-toggle")) {
         sideMenu.classList.remove("side-menu-toggle");
     }
+}
+
+function slideForward(event) {
+    slideFactor = (slideFactor + 1) % carouselImages.length;
+    carouselSlider.style.transform = `translateX(-${carouselWidth * slideFactor}px)`;
+    console.log(slideFactor);
+}
+
+function slideBackward(event) {
+    slideFactor = (slideFactor + (carouselImages.length - 1)) % carouselImages.length;
+    carouselSlider.style.transform = `translateX(-${carouselWidth * slideFactor}px)`;
+    console.log(slideFactor);
+
 }
