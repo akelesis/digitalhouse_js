faa793fbb54b9c2eae6402b3bb96ac33ff2ca427
Ricardo O. Mainente <ricardo.o.mainente@gmail.com>
2020-08-02 19:22:33 -0300

    menu lateral_VFinal

---
+++ main.js
@@ -0,0 +1,9 @@
+dh_menu_btn.onclick = function() {
+    var div = document.getElementById('sidebar');
+    div.style.display = 'block';
+};
+
+dh_closeMenu_btn.onclick = function() {
+    var div = document.getElementById('sidebar');
+    div.style.display = 'none';
+};
