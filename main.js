1762ef733b7cf02075d0f520c54fd7ee8bec7abf
Rafael Santos <rafaneng@gmail.com>
2020-08-02 20:51:38 -0300

    insert books by JS implemented

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const dhMenuBtn = document.getElementById("dh_menu_btn");
 const dhMenuNavegacao = document.querySelector(".menu");
 
 var menuNavegacao = false;
+dhMenuBtn.onclick = menuSidebar;
+
 
 crPrevious.onclick = carrouselPrevious;
 crNext.onclick = carrouselNext;
-dhMenuBtn.onclick = menuSidebar;
+
 
 function carrouselPrevious(){
     dhCarrousel.style.backgroundImage = "url(./img/c1.jpg)";
@@ -28,3 +30,59 @@ function menuSidebar(){
     }
 
 }
+
+function addBooks(){
+
+    
+
+}
+
+let books = {
+    book0: {
+        title: "Livro 1",
+        subtitle: "Descrição do Livro 1: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste corrupti error perspiciatis repellat!",
+        src: "./img/l0.png"
+    },
+    book1: {
+        title: "Livro 2",
+        subtitle: "Descrição do Livro 2: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste corrupti error perspiciatis repellat!",
+        src: "./img/l1.png"
+    },
+    book2: {
+        title: "Livro 3",
+        subtitle: "Descrição do Livro 3: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste corrupti error perspiciatis repellat!",
+        src: "./img/l2.png"
+    },
+    book3: {
+        title: "Livro 4",
+        subtitle: "Descrição do Livro 4: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste corrupti error perspiciatis repellat!",
+        src: "./img/l3.png"
+    },
+    book4: {
+        title: "Livro 5",
+        subtitle: "Descrição do Livro 5: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste corrupti error perspiciatis repellat!",
+        src: "./img/l4.png"
+    },
+    book5: {
+        title: "Livro 6",
+        subtitle: "Descrição do Livro 6: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste corrupti error perspiciatis repellat!",
+        src: "./img/l5.png"
+    }
+}
+
+const cards = document.getElementsByClassName("card");
+
+    for(let i=0; i < cards.length; i++){
+        let imgCards = cards[i].children[0];
+        let divCards = cards[i].children[1];
+        let titleBook = divCards.getElementsByTagName("h2")
+        let subtitleBook = divCards.getElementsByTagName("p")
+
+        let imgCardsSrc = books["book"+[i]].src;
+
+        imgCards.setAttribute("src", imgCardsSrc);
+        titleBook[0].textContent = books["book"+[i]].title;
+        subtitleBook[0].textContent = books["book"+[i]].subtitle;
+
+    }
+
