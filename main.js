a065e1dc395b23009264513b0c78fadb0de51fc5
Hakney <hakneey@gmail.com>
2020-08-02 14:33:53 -0300

    add carrosel

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 // Add event in the dh_menu_btn
-document.querySelector("#dh_menu_btn").addEventListener("click", expandirMenuLateral)
+document.querySelector("#dh_menu_btn").addEventListener("click", expandirMenuLateral);
+
 let navbar = document.querySelector(".dh_menu_lateral");
 let body = document.querySelector("#body");
+let img = document.querySelector("#carrosel_img");
+
+//Carrosel
+const img_carrousel = ['./img/carrousel_1.jpg', './img/carrousel_2.jpg', './img/carrousel_3.jpg'];
+
+let contador = 1;
+function carrosel(){
+    setInterval(function(){
+        img.setAttribute("src", img_carrousel[contador]);
+        contador < img_carrousel.length - 1 ? contador++ :contador = 0;
+    }, 10000)
+}
+carrosel();
+//
 
 // start expanded menu
 function expandirMenuLateral(){
