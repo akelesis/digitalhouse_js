dcf88807ee5443d023b35d25d2a1ea2bf9e2dc81
Rafael Santos <rafaneng@gmail.com>
2020-08-02 15:20:17 -0300

    menu sidebar implemented

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const dhCarrousel = document.getElementById("dh_carousel");
 const crNext = document.getElementById("cr_next");
 const crPrevious = document.getElementById("cr_previous");
+const dhMenuBtn = document.getElementById("dh_menu_btn");
+const dhMenuNavegacao = document.querySelector(".menu");
+
+var menuNavegacao = false;
 
 crPrevious.onclick = carrouselPrevious;
 crNext.onclick = carrouselNext;
+dhMenuBtn.onclick = menuSidebar;
 
 function carrouselPrevious(){
     dhCarrousel.style.backgroundImage = "url(./img/c1.jpg)";
@@ -11,4 +16,15 @@ function carrouselPrevious(){
 
 function carrouselNext(){
     dhCarrousel.style.backgroundImage = "url(./img/c2.jpg)";
+}
+
+function menuSidebar(){
+    if(menuNavegacao == false){
+        dhMenuNavegacao.style.visibility = "visible";
+        menuNavegacao = true;
+    }else{
+        dhMenuNavegacao.style.visibility = "hidden";
+        menuNavegacao = false
+    }
+
 }
