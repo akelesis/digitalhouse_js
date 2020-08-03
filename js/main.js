fe94211d5197bb106dbd84b116fa9a78492cf69b
YtaloWill <ytalowillgloria@gmail.com>
2020-08-03 15:16:19 -0300

    add show all option

--- js/main.js
+++ js/main.js
@@ -17,18 +17,31 @@ const notFoundTemplate = `<p style="color: red; font-size: 1.5em;">Nenhum livro 
 const getBookTitle = (event) => {
     event.preventDefault()
     const title = document.getElementById("search_bar").value
-    const booksFinded = books.filter((book) => book.title === title)
-    fillShowcase(booksFinded)
+    if (title === ""){
+        fillShowcase(books)
+        return
+    }
+
+    const booksByName = books.filter((book) => book.title === title)
+    fillShowcase(booksByName)
 }
 
 document.getElementById("category").addEventListener("change", () => {
     const category = document.getElementById("category").value
+    if (category === 'todos') {
+        fillShowcase(books)
+        return
+    }
     const booksFinded = books.filter((book) => book.category === category)
     fillShowcase(booksFinded)
 })
 
 document.getElementById("price_range").addEventListener("change", () => {
     const priceRange = document.getElementById("price_range").value
+    if (priceRange === 'todos') {
+        fillShowcase(books)
+        return
+    }
     const booksFinded = books.filter((book) => book.priceRange === priceRange)
     fillShowcase(booksFinded)
 })
