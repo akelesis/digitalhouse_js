a57a1f52563367b1df4ff2ae8c6aab709ffeed9b
Matheus Emanoel <lab.emanoel@gmail.com>
2020-08-03 01:53:41 -0300

    inserted carousel

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 dropDownMenu();
+carousel();
+
 
 // DropDown Menu (dh_menu)
 function dropDownMenu(){
@@ -25,3 +27,54 @@ function dropDownMenu(){
     });
 }
 
+// Carousel
+function carousel(){
+    const items = document.querySelectorAll("#dh_carousel img");
+    let dots;
+    let timerCarousel;
+    let itemIndex = 0; //start item 0
+    
+    loadDotsCarousel();
+    startAutoRotateCarousel();
+
+    function showItem(index){
+        if (index >= items.length) {index = 0}
+        if (index < 0) {index = items.length-1}
+        for (let i of items) {i.classList.remove("show");}
+        for (let i of dots) {i.classList.remove("active");}
+        items[index].classList.add("show");
+        dots[index].classList.add("active");
+    }
+    function manualShowItemCarousel(index){
+        clearAutoRotateCarousel();
+        itemIndex = index;
+        startAutoRotateCarousel();
+    }
+    function startAutoRotateCarousel(){
+        clearAutoRotateCarousel();
+        showItem(itemIndex);
+        timerCarousel = setInterval(function(){
+            itemIndex += 1;
+            if (itemIndex >= items.length) {
+                itemIndex = 0
+            }
+            showItem(itemIndex);
+        },3000);
+    }
+    function clearAutoRotateCarousel(){
+        clearInterval(timerCarousel);
+    }      
+    function loadDotsCarousel(){
+        let dotContainer = document.querySelector("#dh_carousel .dh_dot_container");
+        dotContainer.innerHTML = ` `;
+        for(let i=0; i<items.length; i++){
+            let newDot = document.createElement("span");
+            newDot.setAttribute("class","dot");
+            dotContainer.append(newDot);
+        }
+        dots = document.querySelectorAll("#dh_carousel .dh_dot_container .dot");
+        for(let index = 0; index < dots.length; index++){
+            dots[index].addEventListener("click",function(){manualShowItemCarousel(index);});
+        }
+    }
+}
