da78c036219cf2649120f3a95dc9d6b6e91c5649
Thiago Silva <danielareis.ufabc@gmail.com>
2020-08-02 23:49:08 -0300

    início botão e carrossel

---
+++ main.js
@@ -0,0 +1,9 @@
+// carrossel //
+
+let images = ['img/carrossel_1.png', 'img/carrossel_2.png', 'img/carrossel_3.png', 'img/carrossel_4.png'];
+let i = 0;
+
+setInterval (function carousel () {
+    
+}, 5000);
+
