3d04785c976666f38cbb1356a8482de4ac1e8043
Arthur <arthurgog@hotmail.com>
2020-08-04 17:02:20 -0300

    Agora o load de livros é feito pelo javascript

--- main.js
+++ main.js
@@ -1,12 +1,102 @@
-let card = document.querySelectorAll(".card");
-let overlay = document.querySelectorAll(".overlay");
 let body = document.querySelector("body");
 let btn_side_bar = document.getElementById("dh_menu_btn");
 let side_bar = document.querySelector(".navigation");
 let search_button = document.getElementById("search_button");
 let search_bar = document.getElementById("search_bar");
+let items = document.querySelector(".items");
+const showcase = document.querySelector(".showcase");
+
+
+
+const books_list = [
+    {
+        title: "O Poder do Hábito",
+        image: "./img/OPoderdoHabito1-300x400.jpg",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque istecorrupti error perspiciatis repellat!",
+        category: [
+            "Auto Ajuda"
+        ],
+        price: 25
+    },
+    {
+        title: "Harry Potter e o Prisioneiro de Askaban",
+        image: "./img/Harry-Potter.jpg",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque istecorrupti error perspiciatis repellat!",
+        category: [
+            "Fantasia"
+        ],
+        price: 50
+    },
+    {
+        title: "O Hobbit",
+        image: "./img/O-Hobbit.jpg",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque istecorrupti error perspiciatis repellat!",
+        category: [
+            "Fantasia"
+        ],
+        price: 75
+    },
+    {
+        title: "Percy Jackson e os Olimpianos",
+        image: "./img/Percy-Jackson.jpg",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque istecorrupti error perspiciatis repellat!",
+        category: [
+            "Fantasia"
+        ],
+        price: 15
+    },
+    {
+        title: "O Morro dos Ventos Uivantes",
+        image: "./img/O-morro-dos-ventos-uivantes.jpg",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque istecorrupti error perspiciatis repellat!",
+        category: [
+            "Ficção Científica"
+        ],
+        price: 30
+    },
+    {
+        title: "Don Quixote",
+        image: "./img/Don-Quixote.jpg",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque istecorrupti error perspiciatis repellat!",
+        category: [
+            "Fantasia"
+        ],
+        price: 42
+    }
+]
+
+books_list.forEach(book => {
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    
+    const link = document.createElement("a");
+    link.setAttribute("href","#");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", book.image);
+    
+    const overlay = document.createElement("div");
+    overlay.setAttribute("class", "overlay");
+    
+    const title = document.createElement("h2");
+    title.setAttribute("class", "book_title");
+    title.textContent = book.title;
+    
+    const description = document.createElement("p");
+    description.setAttribute("class", "description");
+    description.textContent = book.description;
+    
+    overlay.appendChild(title);
+    overlay.appendChild(description);
+    link.appendChild(img);
+    link.appendChild(overlay);
+    card.appendChild(link);
+    showcase.appendChild(card);
+});
+
+let card = document.querySelectorAll(".card");
+let overlay = document.querySelectorAll(".overlay");
 let book_titles = document.querySelectorAll(".book_title");
-let items = document.querySelector(".items")
 
 items.addEventListener("wheel", function(event){
     if(event.deltaY > 0){
@@ -56,8 +146,8 @@ search_bar.addEventListener("keyup", function(event){
 })
 
 btn_side_bar.addEventListener("click",function(){
-    if(side_bar.style.height != "260px"){
-        side_bar.style.height = "260px";
+    if(side_bar.style.height != "300px"){
+        side_bar.style.height = "300px";
     }
     else{
         side_bar.style.height = "0%";
