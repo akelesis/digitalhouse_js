4d0811883f6f06fcc79539b26ad42d31141f6a2a
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-08-04 03:39:58 -0300

    declarative carousel

--- main.js
+++ main.js
@@ -5,95 +5,103 @@ const carouselSection = document.querySelector('section#dh_carousel');
 const carouselLabel = document.querySelector('h2.carousel_label');
 const carouselParagraph = document.querySelector('p.carousel_paragraph');
 
-/* Imagens para 2 padrões de tela */
+/* Conteúdos do carrossel */
 const carouselImgSources = ["./img/carousel/01_1024px.png", "./img/carousel/02_1140px.jpg"];
-
-const carouselLabels = [{label: "Lorem it", content: "A prática cotidiana prova que o surgimento do comércio virtual aponta para a melhoria das diretrizes de desenvolvimento para o futuro.", color:"white"}, {label: "Uti", content:"As experiências acumuladas demonstram que o entendimento das metas propostas aponta para a melhoria do sistema de participação geral.", color:"black"}, {label: "Mier Lorem", content:"Por outro lado, a determinação clara de objetivos exige a precisão e a definição dos modos de operação convencionais.", color:"black"}];
-
-let carouselControl = 0; // Variável de controle de índices dos conteúdos do carrossel(imagem/label)
-let carouselLabelControl = 0;
-let carouselParagraphControl = 0;
-
-function showMenu(){
-
-    if(menuSection.getAttribute('active') === 'true'){
-        
-        return menuSection.setAttribute('active', 'false');
+const carouselTextContents = [
+    {
+        label: "Lorem it", 
+        paragraph: "A prática cotidiana prova que o surgimento do comércio virtual aponta para a melhoria das diretrizes de desenvolvimento para o futuro.", 
+        color:"white"
+    },
+    {
+        label: "Uti", 
+        paragraph:"As experiências acumuladas demonstram que o entendimento das metas propostas aponta para a melhoria do sistema de participação geral.", 
+        color:"black"
     }
+];
 
-    return menuSection.setAttribute('active', 'true');
-}
+/* Variáveis de controle de índices dos conteúdos de carrossel(imagem/label) */
+let carouselControl = 0;
 
 function selectCarouselContents(){
 
-    let source;
-    let label;
-    let contents;
-
     if(carouselControl === carouselImgSources.length){
-
+        
         carouselControl = 0;
     }
-
-    if(window.innerWidth > 720){
-
-        source = carouselImgSources[carouselControl];
-    }
-
-    label = carouselLabels[carouselControl];
     
-    contents = {
-        // Objeto que contém o caminho de imagem para o bg do carrossel e sua respectiva label
-        imgSrc: source,
-        labelContent: label 
-    };
-        
-    carouselControl++;
+    const selectedContents = {
 
-    return contents;
+        imgSrc: carouselImgSources[carouselControl],
+        textContents: carouselTextContents[carouselControl]
+    };
+    
+    carouselControl++;
+    
+    return selectedContents;
 }
 
-const carousel = setInterval(() => {
+function clearElemsTextsContents(){
     
-    const contents = selectCarouselContents();
-    const label = contents.labelContent.label;
-    const paragraph = contents.labelContent.content;
+    [...arguments].forEach(elm => elm.textContent = "");
+}
 
-    carouselLabel.textContent = "";
-    carouselParagraph.textContent = "";
-    carouselSection.style.backgroundImage = `url(${contents.imgSrc})`;
-    carouselSection.style.color = contents.labelContent.color;
-
-    const initiateLabel = setInterval(() => {
-        
-        if(carouselLabelControl !== label.length){
+function swapAttribute(attr, elm, states){
     
-            carouselLabel.textContent += label[carouselLabelControl];
-            carouselLabelControl++;
-        }
-        else{
+    return function(){
+
+        if(elm.getAttribute(attr) === states.on){
             
-            carouselLabelControl = 0;
-            clearInterval(initiateLabel);
+            return elm.setAttribute(attr, states.off);
         }
         
-    }, 300);
+        return elm.setAttribute(attr, states.on);
+    }
+}
 
-    const initiateParagraph = setInterval(() => {
-        
-        if(carouselParagraphControl !== paragraph.length){
-    
-            carouselParagraph.textContent += paragraph[carouselParagraphControl];
-            carouselParagraphControl++;
-        }
-        else{
+function typing(box, text, control = 0){
+
+    return function(){
+
+        if(control !== text.length){
             
-            carouselParagraphControl = 0;
-            clearInterval(initiateParagraph);
+            box.textContent += text[control];
+
+            return typing(box, text, control++);
         }
+    }
+}
+
+function typingAnimation(box, text, time){
+
+    return setInterval(typing(box, text), time);
+}
+
+function setCarouselStyleSection(color, imageSource){
+    
+    carouselSection.style.color = color;
+    carouselSection.style.backgroundImage = `url(${imageSource})`;
+    
+    return carouselSection.style;
+}
+
+function initCarousel(){
+
+    return setInterval(() => {
+
+                const selectedContents = selectCarouselContents();
+
+                setCarouselStyleSection(selectedContents.textContents.color, selectedContents.imgSrc);
+                clearElemsTextsContents(carouselLabel, carouselParagraph);
+                typingAnimation(carouselLabel, selectedContents.textContents.label, 300);
+                typingAnimation(carouselParagraph, selectedContents.textContents.paragraph, 50);
+            
+            }, 10000);
+}
+
+window.addEventListener('load', function(){
         
-    }, 50);
+    menuButton.addEventListener('click', swapAttribute('active', menuSection, {on: 'true', off: 'false'}));
 
-}, 10000);
-
-menuButton.addEventListener('click', showMenu);
+    initCarousel();
+})
