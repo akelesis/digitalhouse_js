7e286fe8ddeb5c649f3253c25d21605ee387eff9
LuizAraujo <arauldesign@gmail.com>
2020-08-04 03:45:29 -0300

    Carrossel adicionado

--- main.js
+++ main.js
@@ -12,3 +12,49 @@ function showHideMenu(){
     }
 }
 
+//CRIANDO CARROSSEL
+
+var imgs = [];
+var slider;
+var imgAtual;
+var imgMax;
+var tempo, tempoTroca;
+
+
+//PRE CARREGAMENTO DAS IMAGENS
+function preCarregamento(){
+    let b =  1;
+    for(var i = 0; i < 5; i++){
+        imgs[i] = new Image();
+        imgs[i].src = "img/books"+b+".jpg";
+        b++;
+    }
+}
+
+//CARREGA IMAGEM PARA O CARROSSEL
+function carregarImg(img){
+    slider.style.background = "url('"+imgs[img].src+"')";
+    slider.style.backgroundSize = "100% 100%";
+}
+
+//INICIA
+function init(){
+    preCarregamento();
+    imgAtual = 0;
+    imgMax = imgs.length - 1;
+    slider = document.getElementById("dh_carousel");
+    carregarImg(imgAtual);
+    tempoTroca = 3000;
+    tempo = setInterval(trocarImagem, tempoTroca); //INTERVALO DE 3 SEGUNDOS
+}
+
+//TROCA IMAGEM DO CARROSSEL
+function trocarImagem(){
+    imgAtual++;
+    if(imgAtual > imgMax)
+        imgAtual = 0;
+    carregarImg(imgAtual);
+}
+
+//inicia ao carregar a janela no navegador
+window.addEventListener("load", init);
