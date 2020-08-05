61c32f09836c90c817615024a642adc4ab061585
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-08-05 00:17:17 -0300

    advanced filters

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const carouselSection = document.querySelector('section#dh_carousel');
 const carouselLabel = document.querySelector('h2.carousel_label');
 const carouselParagraph = document.querySelector('p.carousel_paragraph');
 
+const filtersInputBookName = document.querySelector('input#search_bar');
+const filtersSelectCategory = document.querySelector('select#category');
+const filtersSelectPrice = document.querySelector('select#price_range');
+
 /* Conteúdos do carrossel */
 const carouselImgSources = ["./img/carousel/01_1024px.png", "./img/carousel/02_1140px.jpg"];
 const carouselTextContents = [
@@ -20,18 +24,28 @@ const carouselTextContents = [
     }
 ];
 
+const books = [
+    {name: "Alooo", price: 30.50, category: "Auto Ajuda"},
+    {name: "Olaaaa", price: 22.50, category: "Romance"},
+    {name: "Helloooo", price: 42.50, category: "Auto Ajuda"},
+    {name: "b", price: 25.50, category: "Auto Ajuda"},
+    {name: "c", price: 70.50, category: "Romance"},
+    {name: "d", price: 13.50, category: "Ficção Científica"},
+    {name: "e", price: 37.50, category: "Fantasia"},
+]
+
 /* Variáveis de controle de índices dos conteúdos de carrossel(imagem/label) */
 let carouselControl = 0;
 
 function selectCarouselContents(){
-
+    
     if(carouselControl === carouselImgSources.length){
         
         carouselControl = 0;
     }
     
     const selectedContents = {
-
+        
         imgSrc: carouselImgSources[carouselControl],
         textContents: carouselTextContents[carouselControl]
     };
@@ -39,6 +53,38 @@ function selectCarouselContents(){
     carouselControl++;
     
     return selectedContents;
+}
+
+function actionByPriceAndCategory(initialPrice, finalPrice, category){
+
+    if(category){
+
+        return function(elem){
+    
+            return (elem.price > initialPrice && elem.price < finalPrice) && (elem.category === category);
+        }
+    }
+    else{
+
+        return function(elem){
+    
+            return (elem.price > initialPrice && elem.price < finalPrice);
+        }
+    }
+}
+
+function filterBooksByPriceAndCategory(){
+
+    const filterCategory = filtersSelectCategory.value; 
+    const myPriceRangeArray = filtersSelectPrice.value.split('?');
+    const initialPrice = parseInt(myPriceRangeArray[0]);
+    const finalPrice = parseInt(myPriceRangeArray[1]);
+
+    const filtered = books.filter(actionByPriceAndCategory(initialPrice, finalPrice, filterCategory));
+
+    console.log(filtered);
+
+    return filtered;
 }
 
 function clearElemsTextsContents(){
@@ -102,6 +148,10 @@ function initCarousel(){
 window.addEventListener('load', function(){
         
     menuButton.addEventListener('click', swapAttribute('active', menuSection, {on: 'true', off: 'false'}));
+
+    filtersSelectCategory.addEventListener('change', filterBooksByPriceAndCategory);
+
+    filtersSelectPrice.addEventListener('change', filterBooksByPriceAndCategory);
 
     initCarousel();
 })
