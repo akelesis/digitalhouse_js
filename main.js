9e91c38f3015c3b9660d4c08801efaaf6419c653
Arthur <arthurgog@hotmail.com>
2020-08-04 21:16:29 -0300

    Adicionado modo de Adicionar livros pro meio da barra lateral.

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 let body = document.querySelector("body");
 let btn_side_bar = document.getElementById("dh_menu_btn");
+const btn_book_add_menu = document.getElementById("book_add_menu_button");
+const book_add_menu = document.querySelector(".book_add");
+const btn_book_add = document.getElementById("book_confirm_add");
+const btn_book_cancel = document.getElementById("book_confirm_cancel");
+const book_add_inputs = document.querySelectorAll(".book_input");
+const book_title_add = document.getElementById("book_title_add");
+const book_img_add = document.getElementById("book_img_add");
+const book_description_add = document.getElementById("book_description_add");
 let side_bar = document.querySelector(".navigation");
 let search_button = document.getElementById("search_button");
 let search_bar = document.getElementById("search_bar");
 let items = document.querySelector(".items");
 const showcase = document.querySelector(".showcase");
-
-
 
 const books_list = [
     {
@@ -65,50 +71,62 @@ const books_list = [
     }
 ]
 
-books_list.forEach(book => {
-    const card = document.createElement("div");
-    card.setAttribute("class", "card");
-    
-    const link = document.createElement("a");
-    link.setAttribute("href","#");
 
-    const img = document.createElement("img");
-    img.setAttribute("src", book.image);
-    
-    const overlay = document.createElement("div");
-    overlay.setAttribute("class", "overlay");
-    
-    const title = document.createElement("h2");
-    title.setAttribute("class", "book_title");
-    title.textContent = book.title;
-    
-    const description = document.createElement("p");
-    description.setAttribute("class", "description");
-    description.textContent = book.description;
-    
-    overlay.appendChild(title);
-    overlay.appendChild(description);
-    link.appendChild(img);
-    link.appendChild(overlay);
-    card.appendChild(link);
-    showcase.appendChild(card);
-});
+let book_titles = document.querySelectorAll(".book_title");
+
+function CheckCopies(book){
+    for(let i=0; i < book_titles.length; i++){
+        console.log(book_titles[i].innerText);
+        console.log(book.title)
+        if(book.title == book_titles[i].innerText){
+            return true;
+        }
+    }
+    return false;
+}
+
+function LoadBooks(){
+    books_list.forEach(book => {
+        if(!CheckCopies(book)){
+            const card = document.createElement("div");
+            card.setAttribute("class", "card");
+            
+            const link = document.createElement("a");
+            link.setAttribute("href","#");
+        
+            const img = document.createElement("img");
+            img.setAttribute("src", book.image);
+            
+            const overlay = document.createElement("div");
+            overlay.setAttribute("class", "overlay");
+            
+            const title = document.createElement("h2");
+            title.setAttribute("class", "book_title");
+            title.textContent = book.title;
+            
+            const description = document.createElement("p");
+            description.setAttribute("class", "description");
+            description.textContent = book.description;
+            
+            overlay.appendChild(title);
+            overlay.appendChild(description);
+            link.appendChild(img);
+            link.appendChild(overlay);
+            card.appendChild(link);
+            showcase.appendChild(card);
+        }
+    });
+}
+
+LoadBooks();
+console.log(book_titles[0]);
+
 
 let card = document.querySelectorAll(".card");
 let overlay = document.querySelectorAll(".overlay");
-let book_titles = document.querySelectorAll(".book_title");
-
-items.addEventListener("wheel", function(event){
-    if(event.deltaY > 0){
-        items.scrollBy(1920, 0);
-    }
-    else{
-        items.scrollBy(-1920, 0);
-    }
-})
+book_titles = document.querySelectorAll(".book_title");
 
 function carouselRoll(){
-    console.log(items.scrollLeft);
     if(items.scrollLeft > 3000){
         items.scrollBy(-3840,0);
     }
@@ -137,6 +155,23 @@ function SearchInTitlesAndHide(search){ // Verifica em todos os títulos de livr
     }
 }
 
+function RefreshCards(){
+    card = document.querySelectorAll(".card");
+    overlay = document.querySelectorAll(".overlay");
+    book_titles = document.querySelectorAll(".book_title");
+    for(let i=0; i < card.length; i++){
+        card[i].addEventListener("mouseover", function(){
+            overlay[i].style.color = "#EEE0CB";
+            overlay[i].style.backgroundColor = "#f71735e8";
+            overlay[i].style.top = "0";
+        })
+        card[i].addEventListener("mouseout", function(){
+            overlay[i].style.color = "transparent";
+            overlay[i].style.backgroundColor = "transparent";
+            overlay[i].style.top = "20px";
+        })
+    }
+}
 
 search_button.addEventListener("click",function(){
     SearchInTitlesAndHide(search_bar.value);
@@ -148,20 +183,56 @@ search_bar.addEventListener("keyup", function(event){
 btn_side_bar.addEventListener("click",function(){
     if(side_bar.style.height != "300px"){
         side_bar.style.height = "300px";
+        book_add_menu.style.height = "0%";
     }
     else{
         side_bar.style.height = "0%";
     }
 })
-for(let i=0; i < card.length; i++){
-    card[i].addEventListener("mouseover", function(){
-        overlay[i].style.color = "#EEE0CB";
-        overlay[i].style.backgroundColor = "#f71735e8";
-        overlay[i].style.top = "0";
-    })
-    card[i].addEventListener("mouseout", function(){
-        overlay[i].style.color = "transparent";
-        overlay[i].style.backgroundColor = "transparent";
-        overlay[i].style.top = "20px";
-    })
+btn_book_add_menu.addEventListener("click",function(){
+        book_add_menu.style.height = "300px";
+        side_bar.style.height = "0%";
+})
+btn_book_cancel.addEventListener("click",function(){
+    book_add_menu.style.height = "0%";
+    EmptyInputs()
+})
+
+function IsNotEmpty(){
+    for(let i=0; i < book_add_inputs.length; i++){
+        if(book_add_inputs[i].value.length == 0){
+            return false
+        }
+    }
+    return true
 }
+
+function EmptyInputs(){
+    for(let i=0; i < book_add_inputs.length; i++){
+        book_add_inputs[i].value = "";
+    }
+}
+
+btn_book_add.addEventListener("click",function(){
+    if(!IsNotEmpty()){
+        alert("Preencha todos os campos!")
+    }
+    else{
+        const book = {
+            title: book_title_add.value,
+            image: book_img_add.value,
+            description: book_description_add.value,
+            category: [
+                "Auto Ajuda"
+            ],
+            price: 25
+        }
+        books_list.push(book);
+        LoadBooks();
+        RefreshCards();
+        book_add_menu.style.height = "0%";
+        alert("Livro adicionado!")
+        EmptyInputs()
+    }
+})
+
