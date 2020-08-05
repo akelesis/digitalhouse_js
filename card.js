2d7815732ec9782adfc66d26d4862a860c87a160
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-08-05 16:03:08 -0300

    deleted main.js; implemented filterByName

--- card.js
+++ card.js
@@ -1,30 +1,45 @@
+const listBooks = []
+const endpoint = './data/books.json'
 const cardSection = document.getElementById('card-section')
+const search_bar = document.getElementById("search_bar")
+
+fetch(endpoint)
+.then(status => status.json())
+.then(data => listBooks.push(...data))
+.then(() => {
+    for (i=0; i<listBooks.length; i++){
+        cardSection.innerHTML += cards(listBooks[i])
+        }
+    }
+)
 
 function cards(json){
     return `
     <div class="card" style="width: 18rem">
-        <img style="width:100%" src="./img/books/${json.img}" alt="Capa do livro ${json.name}">
-        <div class="container">
-            <p class="">${json.name}</p>
-            <p class="">Preço: R$${json.price}</p>    
-            <p class="">Categoria: ${json.category}</p> 
-            <p class="">Descrição: ${json.description}</p>
-        </div> 
+    <img style="width:100%" src="./img/books/${json.img}" alt="Capa do livro ${json.name}">
+    <div class="container">
+    <p class=""><b>${json.name}</b></p>
+    <p class="">Preço: R$${json.price}</p>    
+    <p class="">Categoria: ${json.category}</p> 
+    <p class="">Descrição: ${json.description}</p>
+    </div> 
     </div>`
 }
 
-const books = async () => {
-    try{
-        const response = await fetch('./data/books.json')
-        if(!response.ok) throw `com o status: ${response.status}`
-        const json = await response.json()
-        console.log(json)
-        for (let i=0; i<=json.length; i++){
-            cardSection.innerHTML += cards(json[i])
-        }
-    }catch(e){
-        console.log('Erro ao acessar o array')
+
+const filterByName = () => {
+    const regex = new RegExp(search_bar.value, 'gi')
+    const newList = listBooks.filter(book => {
+        return book.name.match(regex)
+    })
+    for (i=0; i<newList.length; i++){
+        cardSection.innerHTML = cards(listBooks[i])
     }
 }
 
-books()
+const filterByCategory = (listBooks) => {
+
+}
+
+const filterByPrice = (listBooks) => {
+}
