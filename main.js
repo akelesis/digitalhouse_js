169d8fb5c5ee7d86be5991e8a028539ca6d4e6cb
Felipe Oliveira <mec.foliveira@hotmail.com>
2020-08-04 22:46:41 -0300

    insercao livros dinamicamente

--- main.js
+++ main.js
@@ -4,56 +4,64 @@ let livros=[
     editora:"Editora 01",
     preco:"R$ 20,00",
     categoria:"Fantasia",
-    imagem:"img/a_porta_encantada.jpg"},
+    imagem:"img/a_porta_encantada.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
     {titulo:"A profecia de Mídria",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/a_profecia_de_midria.jpg"},
+    imagem:"img/a_profecia_de_midria.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
-    {titulo:"A profecia de Mídria",
+    {titulo:"Artificial Condition",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/artificial_condition.jpg"},
+    imagem:"img/artificial_condition.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
-    {titulo:"A profecia de Mídria",
+    {titulo:"Herói nas Highlands",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/heroi_nas_highlands.jpg"},
+    imagem:"img/heroi_nas_highlands.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
-    {titulo:"A profecia de Mídria",
+    {titulo:"Jardins da Lua Volume Um",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/jardins_da_lua_volume_um.jpg"},
+    imagem:"img/jardins_da_lua_volume_um.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
-    {titulo:"A profecia de Mídria",
+    {titulo:"O poder do Hábito",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/o_poder_do_habito.jpg"},
+    imagem:"img/o_poder_do_habito.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
-    {titulo:"A profecia de Mídria",
+    {titulo:"Os sete hábitos das pessoas altamente eficazes",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/os_sete_habitos_das_pessoas_altamente_eficazes.jpg"},
+    imagem:"img/os_sete_habitos_das_pessoas_altamente_eficazes.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"},
 
-    {titulo:"A profecia de Mídria",
+    {titulo:"Um casamento conveniente",
     autor:"Felipe 02",
     editora:"Editora 02",
     preco:"R$ 30,00",
     categoria:"Fantasia",
-    imagem:"img/um_casamento_conveniente.jpg"}
+    imagem:"img/um_casamento_conveniente.png",
+    descricao: "Lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun"}
 ]
 
 let tempoSlide = 3000;
@@ -132,3 +140,47 @@ function setaImagem(){
 }
 
 window.addEventListener("load",setaImagem,false);
+
+{/* <div class="card">
+    <img src="./img/300x400.png">
+    <div class="overlay">
+        <h2>Texto Exemplo</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati minus eaque quos assumenda.
+            Culpa commodi repudiandae asperiores ipsa hic, dicta cumque earum omnis aperiam eaque iste
+            corrupti error perspiciatis repellat!</p>
+    </div>
+</div> */}
+
+let showcase = document.getElementsByClassName("showcase");
+
+function addBookShowcase(){
+    for(i=0; i<livros.length; i++){
+        let divCard = document.createElement("div");
+        divCard.setAttribute("class", "card");
+
+        let imgCapa=document.createElement("img");
+        imgCapa.setAttribute("src", livros[i].imagem);
+
+        let overlayDiv = document.createElement("div");
+        overlayDiv.setAttribute("class", "overlay");
+
+        let bookTitle = document.createElement("h2");
+        let titleContent = document.createTextNode(livros[i].titulo)
+        bookTitle.appendChild(titleContent);
+
+        let descricaoLivro =document.createElement("p");
+        let descContent = document.createTextNode(livros[i].descricao)
+        descricaoLivro.appendChild(descContent);
+
+        showcase[0].appendChild(divCard);
+
+        divCard.appendChild(imgCapa);
+        
+        divCard.appendChild(overlayDiv);
+        
+        overlayDiv.appendChild(bookTitle);
+        overlayDiv.appendChild(descricaoLivro);
+    }
+}
+
+addBookShowcase();
