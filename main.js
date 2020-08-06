2cddbbb61ce56a6f09d52d5c44c72646a08bcc7f
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2020-08-06 09:45:40 -0300

    side menu finished

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let position = 0;
 
 const carouselNext = () => {
   if (position/deviceWidth === 2) {
+    position = 0;
+    carousel.scrollTo(position, 0);
     return;
   }
   position += deviceWidth;
@@ -15,6 +17,8 @@ const carouselNext = () => {
 
 const carouselBack = () => {
   if (position/deviceWidth === 0) {
+    position += 2 * deviceWidth;
+    carousel.scrollTo(position, 0);
     return;
   }
   position -= deviceWidth;
@@ -26,3 +30,24 @@ carouselNextButton.onclick = carouselNext;
 
 const carouselBackButton = document.getElementById('carousel-back');
 carouselBackButton.onclick = carouselBack;
+
+const sideMenuButton = document.getElementById('dh_menu_btn');
+const dismissSideMenuButton = document.getElementById('dismiss-dh_menu-btn');
+const sideMenu = document.getElementById('side-menu');
+
+let sideMenuOpen = false;
+
+sideMenuButton.addEventListener('click', () => {
+  if (sideMenuOpen === false) {
+    console.log('oi')
+    sideMenu.style.right = '0';
+    sideMenuOpen = true;
+  }
+});
+
+dismissSideMenuButton.addEventListener('click', () => {
+  if (sideMenuOpen) {
+    sideMenu.style.right = '-350px';
+    sideMenuOpen = false;
+  }
+});
