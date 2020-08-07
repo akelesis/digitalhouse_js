edf6fc810d3b1df550bf219f0408aff146303fa1
David Kalil Braga <dmmd10@hotmail.com>
2020-08-07 18:18:50 -0300

    Exercicio completo

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const body = document.querySelector("body")
     // const menu = document.getElementById("menuLateral");
 
 function openCloseMenu() {
-    var menu = document.getElementById("menu");
+    var menu = document.getElementById("menuLateral");
     var overlay = document.getElementById("menu_overlay");
     var styles = window.getComputedStyle(menu);
     console.log("fui clicado")
@@ -24,23 +24,15 @@ function openCloseMenu() {
     }
 }
 
-// function ativaBotaoMenu() {
 
-//     var styles = window.getComputedStyle(menu);
-
-//     if (styles.right === "-270px") {
-//         menu.style.right = "0px";
-//     } else {
-//         menu.style.right = "-270px";
-//     }
-
-// }
 
 botao.addEventListener("click", function() {
 
     // ativaBotaoMenu();
     openCloseMenu();
 })
+
+
 
 
 function mudaImagem() {
@@ -68,12 +60,12 @@ function mudaImagem() {
 setInterval(mudaImagem, 5000);
 
 var livros = [
-    { titulo: "A Garota do Lago", desc: "Charlie Donlea", url: "./img/img1.jpg" },
-    { titulo: "Essa Gente", desc: "Chico Buarque", url: "./img/img2.jpg" },
-    { titulo: "O Conta da Aia", desc: "Margaret Atwood", url: "./img/img3.jpg" },
-    { titulo: "O Homem de Giz", desc: "C.J. Tudor", url: "./img/img4.jpg" },
-    { titulo: "Anne de Green Gables", desc: "Montgomery Lucy Maud", url: "./img/img5.jpg" },
-    { titulo: "As Aventuras de Mike", desc: "Gabriel Dearo e Manu Digillio", url: "./img/img6.jpg" },
+    { titulo: "A Garota do Lago", desc: "Charlie Donlea", url: "./img/img1.jpg", preco: 49.90, categoria: "Auto Ajuda" },
+    { titulo: "Essa Gente", desc: "Chico Buarque", url: "./img/img2.jpg", preco: 59.90, categoria: "Fantasia" },
+    { titulo: "O Conta da Aia", desc: "Margaret Atwood", url: "./img/img3.jpg", preco: 69.90, categoria: "Romance" },
+    { titulo: "O Homem de Giz", desc: "C.J. Tudor", url: "./img/img4.jpg", preco: 9.99, categoria: "Romance" },
+    { titulo: "Anne de Green Gables", desc: "Montgomery Lucy Maud", url: "./img/img5.jpg", preco: 29.90, categoria: "Ficção Científica" },
+    { titulo: "As Aventuras de Mike", desc: "Gabriel Dearo e Manu Digillio", url: "./img/img6.jpg", preco: 19.90, categoria: "Ficção Científica" },
 ];
 
 searchBar.getAttribute("value");
@@ -95,10 +87,72 @@ busca.addEventListener("click", function() {
     console.log(resultadoBusca);
 
     listarLivros(resultadoBusca);
-
+    //filtraCategoria();
 
 
 })
+
+function filtraCategoria() {
+    limpaPesquisa();
+    var categoria = document.getElementById("category");
+
+    console.log(categoria.value)
+    var categoriaBusca = categoria.value;
+    console.log("busca: " + categoriaBusca);
+
+    if (categoriaBusca.length == 0) {
+        resultadoBusca = "";
+    } else {
+
+        var resultadoBusca = livros.filter(function(livros) {
+            return (livros.categoria.toUpperCase() == categoriaBusca.toUpperCase());
+        });
+    }
+    console.log(resultadoBusca);
+    listarLivros(resultadoBusca);
+}
+
+function filtraPreco() {
+    limpaPesquisa();
+    var faixaPreco = document.getElementById("price_range");
+    console.log(faixaPreco.value)
+    var faixaPrecoBusca = faixaPreco.value;
+    console.log("busca: " + faixaPrecoBusca);
+
+    if (faixaPrecoBusca.length == 0) {
+        resultadoBusca = "";
+    } else {
+
+        var resultadoBusca = livros.filter(function(livros) {
+
+            switch (faixaPrecoBusca) {
+                case 'barato':
+                    console.log('barato');
+                    return (livros.preco >= 0 && livros.preco <= 20);
+                    break;
+                case 'medio':
+                    console.log('medio');
+                    return (livros.preco > 20 && livros.preco <= 40);
+                    break;
+                case 'caro':
+                    console.log('caro');
+                    return (livros.preco > 40 && livros.preco <= 60);
+                    break;
+                case 'mais caro':
+                    console.log('mais caro');
+                    return (livros.preco > 60);
+                    break;
+                default:
+                    console.log('nao deveria cair aqui');
+            }
+
+        });
+    }
+    console.log(resultadoBusca);
+    listarLivros(resultadoBusca);
+
+
+}
 
 function limpaPesquisa() {
     var Cards = document.getElementsByClassName("card");
@@ -110,8 +164,6 @@ function limpaPesquisa() {
 
 function listarLivros(arrayLivros) {
 
-
-
     arrayLivros.forEach(livro => {
 
         var livroBox = document.createElement("div");
@@ -121,6 +173,8 @@ function listarLivros(arrayLivros) {
                     <div class="overlay">
                         <h2 class="formatoTitulo">${livro.titulo}</h2>
                         <p class="formatoAutor">${livro.desc}</p>
+                        <br>
+                        <p class="formatoTitulo">R$ ${livro.preco}</p>
                     </div>`
         bookCase.appendChild(livroBox);
 
@@ -133,4 +187,5 @@ function listarLivros(arrayLivros) {
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM completamente carregado e analisado");
     listarLivros(livros);
+
 });
