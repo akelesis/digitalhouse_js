d08c1adca3d62d61784b96f5e6dfe5b751abee40
08pixels <luizrodrigomarques@gmail.com>
2020-08-08 19:13:44 -0300

    feat: search by price and term & side menu

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
-const dataset    = [];
 const dictionary = [
   'fantasia', 'ajuda', 'ficcao', 'romance',
   'amor', 'odio', 'python', 'javascript',
   'religiao', 'java'
 ];
+let dataset    = [];
 
-const almostRandomWord = ( arr ) => {
+const asyncQuery = (query, callback) => {
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+  dataset = [];
+
+  $.ajax(URL, {
+    type: "GET",
+    statusCode: {
+      200: response => {
+        (response.items).map( infoBook => {
+
+          const { title,
+                  description,
+                  imageLinks: { thumbnail },
+                  pageCount: fictitiousPrice } = infoBook.volumeInfo
+  
+          const currentBook = {
+            title,
+            description,
+            thumbnail,
+            fictitiousPrice: fictitiousPrice / 10
+          }
+  
+          if ( fictitiousPrice )
+            dataset.push(currentBook);
+        })
+
+        callback(dataset);
+      }
+  
+    }
+  });
+}
+
+const almostRandomWord = (arr) => {
   const size = arr.length - 1;
   const index = Math.trunc( Math.random() * size );
-  console.log(arr[index])
+
   return arr[index];
 }
 
-const URL = `https://www.googleapis.com/books/v1/volumes?q=${ almostRandomWord( dictionary ) }`
-
-const renderBooks = ( dataset ) => {
-  const showcaseElement = $('.showcase').empty()
-
+const renderBooks = (dataset) => {
+  const showcaseElement = $('.showcase').empty();
+  
   dataset.map( book => {
     showcaseElement.append(`
     <div class="card">
@@ -28,7 +59,8 @@ const renderBooks = ( dataset ) => {
     `)
   })
 }
-const renderCarousel = ( dataset ) => {
+
+const renderCarousel = (dataset) => {
   const size = dataset.length;
   let ind    = 0;
 
@@ -37,7 +69,7 @@ const renderCarousel = ( dataset ) => {
     for ( let i = 0; i < 5; ++i ) {
       carouselElement.append(`
       <div class="carousel_item">
-        <img src=${ dataset[ (ind + i) % size ].thumbnail } width=250 height=350>
+        <img src=${ dataset[ (ind + i) % size ].thumbnail } width=200 height=300>
       </div>
       `)
     }
@@ -46,35 +78,49 @@ const renderCarousel = ( dataset ) => {
   }
 }
 
-$.ajax(URL, {
-  type: "GET",
-  statusCode: {
-    200: response => {
-      (response.items).map( infoBook => {
+const handleMenu = () => {
+  const menuElement =  $("#menu")[0]
 
-        const { title,
-                description,
-                imageLinks: { thumbnail },
-                pageCount: fictitiousPrice } = infoBook.volumeInfo
+  if ( menuElement.className === "opened" ) {
+    menuElement.className = "closed";
+  } else {
+    menuElement.className = "opened";
+  } 
+}
 
-        const currentBook = {
-          title,
-          description,
-          thumbnail,
-          fictitiousPrice: fictitiousPrice / 10
-        }
+const handleSearch = () => {
+  const query = $("#search_bar")[0].value;
+  
+  asyncQuery(query, (dataset) => {
+    handleSearchPrice();
+  })
+}
 
-        if ( fictitiousPrice )
-          dataset.push( currentBook );
-      })
+const handleSearchPrice = () => {
+  const priceRange = $("#price_range option:selected").val()
 
-      const handleCarousel = renderCarousel( dataset );
-      handleCarousel();
-      setInterval( handleCarousel, 1500 );
+  const filtered = dataset.filter(book => {
+    if ( priceRange === 'barato' )
+      return book.fictitiousPrice >= 1 && book.fictitiousPrice <= 20
+    if ( priceRange === 'medio')
+      return book.fictitiousPrice > 20 && book.fictitiousPrice <= 40
+    if ( priceRange === 'caro')
+      return book.fictitiousPrice > 40 && book.fictitiousPrice <= 60
+    else if ( priceRange === 'mais caro' )
+      return book.fictitiousPrice > 60;
+    return true;
+  });
 
-      renderBooks( dataset );
-    }
+  renderBooks(filtered);
+}
 
-  }
-});
+const initialWord = almostRandomWord(dictionary);
+
+asyncQuery(initialWord, (dataset) => {
+  const handleCarousel = renderCarousel(dataset);
+  handleCarousel();
+  
+  setInterval(handleCarousel, 1500);
+  renderBooks(dataset);
+})
 
