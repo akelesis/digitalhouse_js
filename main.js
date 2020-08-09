e63f7df5f99b392d0994099ae54dcb6e909e275d
Maina <fisionainalex@gmail.com>
2020-08-09 01:35:56 -0300

    Implementados: menu lateral e busca por nome

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// const btnMenu = document.getElementById("#dh_menu_btn");
-// const mnLat = document.getElementById("#menulat");
-// btnMenu.addEventListener("click", function(){
-//     mnLat.setAttribute("right", "15px");
-// })
+const btnMenu = document.getElementById("dh_menu_btn");
+const mnLat = document.getElementById("menulat");
+mnLat.setAttribute("style", "visibility: hidden")
+function aparecerMenu(){
+    console.log ("ta rolando")
+    if (mnLat.style.visibility == "visible"){
+        mnLat.setAttribute("style", "visibility: hidden")
+    } else if (mnLat.style.visibility == "hidden"){
+        mnLat.setAttribute("style", "visibility: visible")
+    }
+}
+btnMenu.addEventListener("click", aparecerMenu)
+
 
 let livro1={
     nome: "As Cidades Invisíveis" ,
@@ -44,6 +52,12 @@ let livros=[livro1, livro2, livro3, livro4, livro5, livro6]
 let capas=livros.map(function(l){
     return l.capa;
 });
+let nomes=livros.map(function(n){
+    return n.nome;
+});
+let precos=livros.map(function(p){
+    return p.preco;
+});
 let imgcapa1 = document.getElementById("img1car")
 let imgcapa2 = document.getElementById("img2car")
 let imgcapa3 = document.getElementById("img3car")
@@ -60,4 +74,64 @@ function carousel(){
         cont3<(livros.length-1)? cont3++ : cont3=0;
     }, 3000);
 }
-carousel()
+carousel();
+
+const qualLivro = document.getElementById("search_bar");
+const btBusca = document.getElementById("search_button");
+const container = document.getElementById("showc")
+
+btBusca.addEventListener("click", function(){
+    container.innerHTML="";
+    const resposta = livros.filter(nm => {
+        return (nm.nome).toLowerCase() == (qualLivro.value).toLowerCase()
+    })
+    resposta.forEach(function(lv){
+    let h3d = document.createElement("h3");
+    let textoh3d = document.createTextNode("Encontramos seu livro!")
+    let pPreco = document.createElement("p")
+    let textoPreco = document.createTextNode("O preço dele é " + lv.preco)
+    let imgh3d = document.createElement("img")
+    imgh3d.setAttribute("src", lv.capa)
+    imgh3d.setAttribute("style", "width: 300px")
+    pPreco.appendChild(textoPreco)
+    h3d.setAttribute("style", "fontSize: 20px")
+    h3d.appendChild(textoh3d);
+    h3d.appendChild(pPreco);
+    document.getElementById("showc").appendChild(h3d);
+    document.getElementById("showc").appendChild(imgh3d);
+    })
+})    
+
+
+
+
+  
+
+//     return nomes.indexOf("qualLivro")
+//     }
+// if (buscaPeloNome == -1) {
+//     function sumir (item){
+//         item.setAttribute("style", "visibility: hidden")
+//     }
+//     someCard.forEach(sumir)
+//     let h3 = document.createElement("h3");
+//     let textoh3 = document.createTextNode("Seu livro não foi encontrado")
+//     h3.appendChild(textoh3);
+//     document.getElementById("showc").appendChild(h3);
+// } else {
+//     function sumir3 (item3){
+//         item3.setAttribute("style", "visibility: hidden")
+// //     }
+//     // someCard.forEach(sumir3)
+//     let h3d = document.createElement("h3");
+//     let textoh3d = document.createTextNode("Encontramos seu livro!")
+//     let imgh3d = document.createElement("img")
+//     imgh3d.setAttribute("src", capas[buscaPeloNome])
+//     h3d.setAttribute("style", "fontSize: 20px")
+//     h3d.appendChild(textoh3d);
+//     document.getElementById("showc").appendChild(h3d);
+//     document.getElementById("showc").appendChild(imgh3d);
+// // }
+
+
+
